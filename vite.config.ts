c2c574cf6b7a0a8193fcda6db0cb38931d6b4a5e
skillobyte <raphaelus@gmail.com>
2026-04-28 01:09:46 -0300

    git config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import path from 'path';
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
@@ -8,13 +7,6 @@ export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
-  ],server: {
-        port: 62307,
-    },
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "src")
-        }
-    },
-    base: '/my-workout-session/',
+  ],
+  base: '/my-workout-session/',
 })
