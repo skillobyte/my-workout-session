4f740e8f9543faffbc76b7366f50aecef890fcab
skillobyte <raphaelus@gmail.com>
2026-04-28 00:19:05 -0300

    config to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
+import path from 'path';
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'build', 
-    emptyOutDir: true,
-  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
-  ],
+  ],server: {
+        port: 62307,
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src")
+        }
+    },
     base: '/my-workout-session/',
 })
